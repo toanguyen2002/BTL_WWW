dc2200be28649c68589c47f99c2caa07c0fd33a7
Quang Toan <toanguyen200220@gmail.com>
2023-11-16 13:31:21 +0700

    update data order

---
+++ src/main/java/www/btl/Controller/OrderController.java
@@ -0,0 +1,14 @@
+package www.btl.Controller;
+
+import www.btl.Entity.Order;
+
+import javax.transaction.Transactional;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderController {
+	 
+}
